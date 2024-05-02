3427b1c81d7bb30bd40be43c515ac2ae0c7895bd
book3039 <book3039@knou.ac.kr>
2024-05-02 13:39:54 +0900

    (feat): add notification tap feature

--- app/src/main/java/com/syncrown/android_notification_demo/MainActivity.kt
+++ app/src/main/java/com/syncrown/android_notification_demo/MainActivity.kt
@@ -2,15 +2,14 @@ package com.syncrown.android_notification_demo
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private val channelID = "com.syncrown.android_notification_demo.channel1"
@@ -29,12 +28,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification() {
         val notificationId = 45
+        val tapResultIntent = Intent(this, SecondActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notification = NotificationCompat.Builder(this@MainActivity, channelID)
             .setContentTitle("Demo Title")
             .setContentText("This is a demo notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(notificationId, notification)
     }
